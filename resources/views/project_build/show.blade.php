d972941684782d220ee1071ca1c47582db53840b
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-08-20 16:47:56 +0600

    fx show

--- resources/views/project_build/show.blade.php
+++ resources/views/project_build/show.blade.php
@@ -1,13 +1,62 @@
 @extends('layouts.app')
 @section('content')
     <div class="container pt-3">
-        <div class="col-12">
-            <h2 class="text-center  py-3 font-weight-bold">Поэтапная приемка</h2>
-            <div class="text-right mb-2">
-                <button data-toggle="modal" data-target="#addStages" type="button" class="btn btn-dark-green  "
-                        style="padding: 11px 19px;margin-top: 15px;">Добавить этап
-                </button>
+        <div class="row">
+            <div class="col-12">
+                <h3 class=" py-3 font-weight-bold mb-0">Название объекта</h3>
             </div>
+            <div class="col-12 bg-form card-body-admin p-3">
+                <div class="row border-bottom">
+                    <div class="col-lg col-12 text-lg-left text-center">
+                        <p class="h6 font-weight-bold ">ФИО:</p>
+                        <p class="text-muted">Николай А.Н</p>
+                    </div>
+                    <div class="col-lg col-12 text-lg-left text-center">
+                        <p class="h6 font-weight-bold ">Вид объекта:</p>
+                        <p class="text-muted">Lorem ipsum dolor sit</p>
+                    </div>
+                    <div class="col-lg col-12 text-lg-left text-center">
+                        <p class="h6 font-weight-bold">Адрес объекта:</p>
+                        <p class="text-muted">Фрунзе 123</p>
+                    </div>
+                    <div class="col-lg col-12 text-lg-left text-center">
+                        <p class="h6 font-weight-bold">Площадь объекта:</p>
+                        <p class="text-muted">123 кв</p>
+                    </div>
+                    <div class="col-lg col-12 text-lg-left text-center">
+                        <p class="h6 font-weight-bold">Примечание:</p>
+                        <p class="text-muted">что-то</p>
+                    </div>
+                </div>
+                <div class=" row ">
+                    <div class="col-lg-3 col-12 text-lg-left text-center pt-4">
+                        <a href="" class="h6 font-weight-bold shadow-sm" style="background-color: #508aeb;color:white;padding: 5px;border-radius: 5px;">Заявление</a>
+                    </div>
+                    <div class="col-lg-3 col-12 text-lg-left text-center pt-4">
+                        <a href="" class="h6 font-weight-bold shadow-sm" style="background-color: #508aeb;color:white;padding: 5px;border-radius: 5px;">АПУ/ИТУ</a>
+                    </div>
+                    <div class="col-lg-3 col-12 text-lg-left text-center pt-4">
+                        <a href="" class="h6 font-weight-bold  shadow-sm" style="background-color: #508aeb;color:white;padding: 5px;border-radius: 5px;">Гос.акт</a>
+                    </div>
+                    <div class="col-lg-3 col-12 text-lg-left text-center pt-4">
+                        <a href="" class=" font-weight-bold shadow-sm" style="background-color: #508aeb;color:white;padding: 5px;border-radius: 5px;">Проект</a>
+                    </div>
+                    <div class="col-lg-4 col-12 text-lg-left text-center pt-4">
+                            <a href="" class=" font-weight-bold " >Разрешение на строительство(положительное разрешение гос.экспертизы)</a>
+                    </div>
+                    <div class="col-lg-4 col-12 text-lg-left text-center pt-4">
+                        <a href="" class=" font-weight-bold ">АКТ оценки соотвествия вводимого в эксплуатацию
+                            завершенного строительства объекта</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-12 row justify-content-between py-2">
+            <h3 class="text-center  py-3 font-weight-bold mb-0">Поэтапная приемка</h3>
+            <button data-toggle="modal" data-target="#addStages" type="button" class="btn btn-dark-green  "
+                    style="padding: 11px 19px;margin-top: 15px;">Добавить этап
+            </button>
         </div>
         <div class="row">
             <div class="col-12 bg-form card-body-admin ">
@@ -16,37 +65,35 @@
                         <p class="font-weight-bold font-small " style="padding-top: 25px;">20.08.2020</p>
                     </div>
                 </div>
-                <div class="justify-content-between row pt-2">
+                <div class="row pt-2">
                     <div class="col-lg-3 col-12 text-lg-left text-center">
-                        <p class="h5 font-weight-bold ">Этап:</p>
+                        <p class="h6 font-weight-bold ">Этап:</p>
                         <p class="text-muted">Котлован</p>
                     </div>
                     <div class="col-lg-6 col-12 text-lg-left text-center">
-                        <p class="h5 font-weight-bold ">Описание:</p>
+                        <p class="h6 font-weight-bold ">Описание:</p>
                         <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
                             architecto
                             asperiores atque consectetur cum in iure numquam pariatur quibusdam, veritatis.</p>
                     </div>
                     <div class="col-lg-3 col-12 text-lg-left text-center">
-                        <p class="h5 font-weight-bold">Документы:</p>
+                        <p class="h6 font-weight-bold">Документы:</p>
                         <p class="text-muted">доки</p>
                     </div>
                 </div>
-                <div class="row justify-content-between pt-3">
+                <div class="row  pt-3">
                     <div class="col-lg-3 col-12 text-lg-left text-center">
-                        <p class="h5 font-weight-bold">Скан.Документа:</p>
+                        <p class="h6 font-weight-bold">Скан.Документа:</p>
                         <p class="">скан</p>
                     </div>
                     <div class="col-lg-6 col-12 text-lg-left text-center">
-                        <p class="h5 font-weight-bold ">Фото объекта:</p>
+                        <p class="h6 font-weight-bold ">Фото объекта:</p>
                         <p class="">картинки</p>
-
                     </div>
                     <div class="col-lg-3 col-12 text-lg-left text-center">
-                        <p class="h5 font-weight-bold ">Примечание:</p>
+                        <p class="h6 font-weight-bold ">Примечание:</p>
                         <p class=" text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus illo
                             nemo unde.</p>
-
                     </div>
                 </div>
             </div>
