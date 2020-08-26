40f7c44f15160bc78492a5e3840544fa730ce862
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-08-26 14:20:48 +0600

    fx button style in main

--- resources/views/blocks/main.blade.php
+++ resources/views/blocks/main.blade.php
@@ -19,9 +19,9 @@
                             </div>
                         </div>
                         <div
-                                class="d-flex col-lg-3 justify-content-lg-end my-2 pr-lg-2 col-12 d-flex justify-content-center  ">
+                                class="d-flex col-lg-3 justify-content-lg-end my-2 pr-lg-2 col-12 d-flex justify-content-center align-items-center ">
                             <a href="{{ route('isp.create.build') }}" type="button"
-                               class="btn btn-outline-default  waves-effect text-right " style="padding: 8px 26px;">
+                               class="btn btn-blue  waves-effect text-right " style="padding: 8px 26px;">
                                 Добавить объект
                             </a>
                         </div>
